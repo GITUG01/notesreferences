97ae6057192e86b882cb17375184aaf6956aead0
Alexandr <sasha-03-30@mail.ru>
2021-09-28 18:51:38 +0300

    attach toolbar and menu

--- app/src/main/java/com/example/notesreferences/MainActivity.java
+++ app/src/main/java/com/example/notesreferences/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.notesreferences;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.notes, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
